32ccf9303cff65bbd5c2adda0cbbf1f9fafef049
brookeojala <brooke.ojala@gmail.com>
2024-01-23 20:26:37 -0800

    fixed but no sound

--- components/Bar.js
+++ components/Bar.js
@@ -1,11 +1,58 @@
 import { Animated, View } from 'react-native'
 import React, { Component } from 'react'
+//import metronone from './metronome.wav'
+import Sound from 'react-native-sound'
+//import MetronomeModule from "react-native-metronome-module";
 
 export default class Bar extends Component {
     constructor(props) {
         super(props);
         this.stretchAnimation = new Animated.Value(240);
+        // this.start = {
+        //     playing: true,
+        //     count: 0,
+        //     bpm: 110,
+        //     beatsPerMeasure: 4,
+        // }
+        // this.metronone = new Sound(metronone);
     }
+    // startStop = () => {
+    //     if (this.state.playing) {
+    //         // Stop the timer
+    //         clearInterval(this.timer);
+    //         this.setState({
+    //             playing: false
+    //         });
+    //     } else {
+    //         // Start a timer with the current BPM
+    //         this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+    //         this.setState({
+    //             count: 0,
+    //             playing: true
+    //             // Play a click "immediately" (after setState finishes)
+    //         }, this.playClick);
+    //     }
+    // }
+    // playClick = () => {
+    //     const { count, beatsPerMeasure } = this.state;
+
+    //     // The first beat will have a different sound than the others
+    //     if (count % beatsPerMeasure === 0) {
+    //         this.metronone.play();
+    //     } else {
+    //         this.metronone.play();
+    //     }
+
+    //     // Keep track of which beat we're on
+    //     this.setState(state => ({
+    //         count: (state.count + 1) % state.beatsPerMeasure
+    //     }));
+    // }
+    // playingMetronome() {
+    // MetronomeModule.setBPM(110);
+    // MetronomeModule.setShouldPauseOnLostFocus(true);
+    // MetronomeModule.start();
+    //}
 
     componentDidMount() {
         Animated.loop(
@@ -25,6 +72,12 @@ export default class Bar extends Component {
                 iterations: this.props.repeat_count
             }
         ).start();
+
+        // if (await MetronomeModule.isPlaying()) {
+        //     const bpm = await MetronomeModule.getBPM();
+        //     console.log(`Metronome playing at ${bpm}bpm!`);
+
+        // }
     }
     componentDidUpdate(prevProps) {
         if ((this.props.color !== prevProps.color)) // check if color changed
@@ -33,6 +86,7 @@ export default class Bar extends Component {
         }
     }
     render() {
+        //this.startStop;
         return (
             <View style={{ ...this.props.style }}>
                 <Animated.View style={{ height: this.stretchAnimation }}>
